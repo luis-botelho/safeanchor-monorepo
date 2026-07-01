3ca7812faeab27f002b2ba79120ca6f4d8f36873
Luis Fellype Botelho <79022593+luis-botelho@users.noreply.github.com>
2026-06-30 22:57:12 -0300

    fix some things

--- apps/backend/src/services/vesselService.js
+++ apps/backend/src/services/vesselService.js
@@ -1,22 +1,5 @@
 import { Vessel } from "../models/vesselModel.js";
 
-// export const getAllVessels = () => {
-//   return [
-//     new Vessel(
-//       1,
-//       "Sea Explorer",
-//       "Lancha",
-//       "Ativa"
-//     ),
-//     new Vessel(
-//       2,
-//       "Ocean Dream",
-//       "Iate",
-//       "Manutenção"
-//     )
-//   ];
-// };
-
 const vessels = [
   {
     id: 1,
