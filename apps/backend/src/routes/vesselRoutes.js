fce59512bfc9d1b5e5190bbb400280aeb6e617b7
Luis Fellype Botelho <79022593+luis-botelho@users.noreply.github.com>
2026-06-30 23:34:36 -0300

    finished sa-017

--- apps/backend/src/routes/vesselRoutes.js
+++ apps/backend/src/routes/vesselRoutes.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   getVessels,
   getVessel,
-  createNewVessel
+  createNewVessel,
+  updateVesselController,
 } from "../controllers/vesselController.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/", getVessels);
 router.post("/", createNewVessel);
 router.get("/:id", getVessel);
+router.put("/:id", updateVesselController)
 
 export default router;
