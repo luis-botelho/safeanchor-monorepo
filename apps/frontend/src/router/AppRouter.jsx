44a4d3523a326b43ef1ffd6da4ebe6c188ef99b6
Luis Fellype Botelho <79022593+luis-botelho@users.noreply.github.com>
2026-08-27 18:37:48 -0300

    refactor(frontend): extract application router

---
+++ apps/frontend/src/router/AppRouter.jsx
@@ -0,0 +1,78 @@
+import { Route, Routes } from "react-router-dom";
+
+import VesselsPage from "../views/VesselsPage";
+import CreateVesselPage from "../views/CreateVesselPage";
+import VesselDetailsPage from "../views/VesselDetailsPage";
+import EditVesselPage from "../views/EditVesselPage";
+
+import MaintenancesPage from "../views/MaintenancesPage";
+import MaintenanceHistoryPage from "../views/MaintenanceHistoryPage";
+import CreateMaintenancePage from "../views/CreateMaintenancePage";
+
+import PreventiveMaintenancesPage from "../views/PreventiveMaintenancesPage";
+import CreatePreventiveMaintenancePage from "../views/CreatePreventiveMaintenancePage";
+import MaintenanceDashboardPage from "../views/MaintenanceDashboardPage";
+
+import CreateChecklistTemplatePage from "../views/CreateChecklistTemplatePage";
+import ChecklistTemplatesPage from "../views/ChecklistTemplatesPage";
+import CreateChecklistExecutionPage from "../views/CreateChecklistExecutionPage";
+import ChecklistExecutionsPage from "../views/ChecklistExecutionsPage";
+
+import InspectionHistoryPage from "../views/InspectionHistoryPage";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      {/* Vessels */}
+      <Route path="/" element={<VesselsPage />} />
+      <Route path="/create" element={<CreateVesselPage />} />
+      <Route path="/vessels/:id" element={<VesselDetailsPage />} />
+      <Route path="/update/:id" element={<EditVesselPage />} />
+
+      {/* Maintenances */}
+      <Route path="/maintenances" element={<MaintenancesPage />} />
+      <Route path="/maintenances/create" element={<CreateMaintenancePage />} />
+      <Route
+        path="/vessels/:id/maintenances"
+        element={<MaintenanceHistoryPage />}
+      />
+
+      {/* Preventive Maintenances */}
+      <Route
+        path="/preventive-maintenances"
+        element={<PreventiveMaintenancesPage />}
+      />
+      <Route
+        path="/preventive-maintenances/create"
+        element={<CreatePreventiveMaintenancePage />}
+      />
+      <Route
+        path="/maintenance-dashboard"
+        element={<MaintenanceDashboardPage />}
+      />
+
+      {/* Checklist Templates */}
+      <Route path="/checklist-templates" element={<ChecklistTemplatesPage />} />
+      <Route
+        path="/checklist-templates/new"
+        element={<CreateChecklistTemplatePage />}
+      />
+
+      {/* Checklist Executions */}
+      <Route
+        path="/checklist-executions"
+        element={<ChecklistExecutionsPage />}
+      />
+      <Route
+        path="/checklist-executions/new"
+        element={<CreateChecklistExecutionPage />}
+      />
+
+      {/* Inspections */}
+      <Route
+        path="/vessels/:id/inspections"
+        element={<InspectionHistoryPage />}
+      />
+    </Routes>
+  );
+}
